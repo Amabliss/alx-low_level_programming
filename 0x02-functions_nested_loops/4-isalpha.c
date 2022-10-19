e4396216547724fe3f775c18e96cff9d799db89c
Amabliss <anukwublessing19@gmail.com>
2022-10-18 20:20:39 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,13 @@
 /**
  * _isalpha - checks for char alphabet
  * @c: the char to be checked
+ *
  * Return: 1 if char is a letter lower or upper. 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
 		return (1);
 	else
 		return (0);
