c76152dba9aaa05e34f0ed82bdec1aa9651c6d84
Amabliss <anukwublessing19@gmail.com>
2022-10-20 12:24:14 -0700

    Debugging 00

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - testing the integer if it prints positive or negative
- *
+ * main - tests function that prints if integer is positive or negative
  * Return: Always 0
  */
 
