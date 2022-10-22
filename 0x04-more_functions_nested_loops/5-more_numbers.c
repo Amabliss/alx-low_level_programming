35bc2b6da58d8bc88fb63cd75130236e9e6613a4
Amabliss <anukwublessing19@gmail.com>
2022-10-21 20:32:54 -0700

    Five

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,14 @@ void more_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		if (num > 9)
+		for (num = 0; num <= 14; num++)
 		{
-			_putchar((num / 10) + '0');
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
 		}
-		_putchar((num % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
