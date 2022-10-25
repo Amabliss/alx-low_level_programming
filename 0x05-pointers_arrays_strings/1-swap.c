884e2c4e23a1494048bcb876bf624d24335eabea
Amabliss <anukwublessing19@gmail.com>
2022-10-24 22:12:20 -0700

    Task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
- /**
-  * swap_int - swaps the values of two integers
-  * @a: int a
-  * @b: int b
-  */
+/**
+ * swap_int - swaps the value of two integers
+ * @a: int a
+ * @b: int b
+ */
 void swap_int(int *a, int *b)
 {
 	int q;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
@@ -12,4 +13,4 @@ char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
 
-#endiI
+#endif
