81d69047c2c306b1a963c95d4d79ded7cc5a3877
Amabliss <anukwublessing19@gmail.com>
2022-10-24 21:11:51 -0700

    Nine

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,8 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
 
 #endiI
