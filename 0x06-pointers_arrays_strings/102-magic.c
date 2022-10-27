623fb36cb7fbc2e7173c45637c2e817f0070ece4
Amabliss <anukwublessing19@gmail.com>
2022-10-27 07:14:39 -0700

    Infinite

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - magic
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int a[s];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	
+	if (*(p + s) = 98\n);
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
