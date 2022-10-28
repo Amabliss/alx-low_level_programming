8bcf2fde28a5611973e7ab5e1021accac48d2e23
Amabliss <anukwublessing19@gmail.com>
2022-10-27 23:07:39 -0700

    Rot

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,18 +5,19 @@
  * @str: string
  * Return: pointer to string
  */
+
 char *cap_string(char *str)
 {
 	int i = 0;
 
 	while (str[i])
 	{
-		while (!(str[i]) >= 'a' && str[i] <= 'z'
-
+		while (!(str[i]) >= 'a' && str[i] <= 'z')
 				i++;
-		            if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t'||	
-				str[i - 1] == '\n'||
+
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
 				str[i - 1] == ',' ||
 				str[i - 1] == ';' ||
 				str[i - 1] == '.' ||
