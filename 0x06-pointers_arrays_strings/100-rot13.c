280e26139f519d6bc8ceeb85861c0420af0942d7
Amabliss <anukwublessing19@gmail.com>
2022-11-03 16:57:46 -0700

    Prime number

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * rot13 - encodes a string into rot13.
@@ -10,8 +9,8 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *a = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	char *b = ("nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM");
 
 	for (i = 0; *(s + i); i++)
 	{
