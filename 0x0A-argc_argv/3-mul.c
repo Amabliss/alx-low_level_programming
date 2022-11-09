65212ff4995a91892a443a63ccbc53b1e8585d59
Amabliss <anukwublessing19@gmail.com>
2022-11-09 15:20:32 -0800

    Last one

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+int check_num(char *str)
+
+{
+	/*Declaring variable*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigest(str()) /*check if str there are digit*/
+				{
+				return (0);
+				}
+				count++;
+				}
+				return (1);
+				}
+				/**
+				 * main - print the number of the program
+				 * @argec: count arguements
+				 * @argv: Arguments
+				 * Return: Always 0 (success)
+				 */
+
+				int main(int argc, char *argu[])
