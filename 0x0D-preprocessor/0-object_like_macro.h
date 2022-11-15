5559b71aab8a36711529a2b501d27e2494e90b85
Amabliss <anukwublessing19@gmail.com>
2022-11-14 17:15:56 -0800

    Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * main - check your code
+ * Return: Always zero
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+this is talking about preprocessors, basically how our codes are being compiled
